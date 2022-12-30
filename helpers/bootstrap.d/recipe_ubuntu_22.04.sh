8fd02ea262afc5707378aa64e2a87abc5761c827
Stefan Schroeder <ondekoza@gmail.com>
2022-12-30 21:19:28 +0100

    more mkdocs extensions

--- helpers/bootstrap.d/recipe_ubuntu_22.04.sh
+++ helpers/bootstrap.d/recipe_ubuntu_22.04.sh
@@ -2,7 +2,9 @@
 echo "This is the recipe for Ubuntu 22.04 LTS."
 echo "This is the foundation for the online version."
 
+echo "Step 1"
 apt update
+echo "Step 2"
 apt -y install texlive-latex-recommended \
 	texlive-latex-base \
 	texlive-fonts-recommended \
@@ -12,10 +14,9 @@ apt -y install texlive-latex-recommended \
 	latex2html \
 	make \
 	latexmk \
-	biber
-	
-apt -y install pandoc mkdocs mkdocs-nature \
+	biber \
+	pandoc mkdocs mkdocs-nature \
 	mkdocs-bootstrap \
 	mkdocs-material-extensions
-
+echo "Step 3"
 pip install mkdocs-windmill
